51eac34b1087e320ac492d1204e104019345802b
searchmoon <moonding901212@gmail.com>
2022-10-13 22:57:54 +0900

    detail page 로직 추가

--- src/pages/DiaryDetail.jsx
+++ src/pages/DiaryDetail.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
 import Header from "../components/Header";
+import { getDiaryItem } from "../features/diarySlice";
 
 function DiaryDetail() {
+  const { id } = useParams();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    console.log(id);
+    dispatch(getDiaryItem(id));
+  }, []);
+
+  const item = useSelector((state) => state.diary.item);
+  console.log("item", item);
+
   return (
     <>
       <Header />
