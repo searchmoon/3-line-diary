3fa228b7e11b7ceda9b9ee8e037f467294b15b66
Rumi <moon901212@gmail.com>
2023-10-20 18:54:49 +0900

    PWA 적용

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <App />
   </Provider>
 );
+
+serviceWorkerRegistration.register();
